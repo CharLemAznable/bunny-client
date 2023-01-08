6ff656a774782ff43e56dcae780cfe07d21383c5
CharLemAznable <545541819@qq.com>
2023-01-08 18:02:56 +0800

    Some refactor.

--- src/main/java/com/github/charlemaznable/bunny/client/config/BunnyClientConfig.java
+++ src/main/java/com/github/charlemaznable/bunny/client/config/BunnyClientConfig.java
@@ -1,15 +1,13 @@
 package com.github.charlemaznable.bunny.client.config;
 
-import com.github.charlemaznable.configservice.apollo.ApolloConfig;
-import com.github.charlemaznable.configservice.diamond.DiamondConfig;
+import com.github.charlemaznable.configservice.Config;
 
-@ApolloConfig(namespace = "BunnyClient", propertyName = "${bunny-client-config:-default}")
-@DiamondConfig(group = "BunnyClient", dataId = "${bunny-client-config:-default}")
+@Config(keyset = "BunnyClient", key = "${bunny-client-config:-default}")
 public interface BunnyClientConfig {
 
-    @DiamondConfig(defaultValue = "http://127.0.0.1:22114/bunny")
+    @Config(defaultValue = "http://127.0.0.1:22114/bunny")
     String httpServerBaseUrl();
 
-    @DiamondConfig(defaultValue = "/bunny")
+    @Config(defaultValue = "/bunny")
     String eventBusAddressPrefix();
 }
